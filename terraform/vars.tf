7f50d250b88f3cbce10118934273f18dd982cb47
Javier Eduardo Marín Fernández <oldschoolxd@gmail.com>
2023-07-16 16:16:34 +0200

    Creación vm inicial tf

---
+++ terraform/vars.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "rg-casopractico2"
+}
+
+variable "location_name" {
+  default = "uksouth"
+}
+
+variable "network_name" {
+  default = "vnet1"
+}
+
+variable "subnet_name" {
+  default = "subnetvm"
+}
